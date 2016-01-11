5a8e97f8f5bd8a471199e472afaea53548894a46
John Bartos <jbartos7@gmail.com>
2016-01-10 22:48:04 -0500

    Highlight current note bar

--- makenoise.js
+++ makenoise.js
@@ -4,33 +4,33 @@
 //#44A340 - 3
 //#1E6823 - 4 (max)
 
-((audioContext, freq, speed) => {
+((audioContext, freq, delay) => {
   'use strict';
   const levels = {"#eeeeee": freq, "#d6e685": freq * (3/2), "#8cc665": freq * (3/2) * 2, "#44a340": freq * (3/2) * 3, "#1e6823": freq * (3/2) * 4};
-  const oscillatorsPerWeek = ([].slice.call(document.getElementsByTagName('g'))).slice(1).map((week) => {
-    return ([].slice.call(week.getElementsByClassName('day'))).map((day) => {
-      return levels[day.attributes.fill.nodeValue];
-    });
-  }).map((bar) => {
-      return createOscillators(audioContext, bar);
-    });
+  const noteBars = ([].slice.call(document.getElementsByTagName('g'))).slice(1).map((week) => {
+    return createOscillators(audioContext, [].slice.call(week.getElementsByClassName('day')));
+  });
 
   let i = 0;
-  for (let oscillators of oscillatorsPerWeek) {
+  for (let noteBar of noteBars) {
     ((j) => {
       setTimeout(() => {
-        for (let oscillator of oscillators) {
-          oscillator.start(0);
+        for (let node of noteBar) {
+          const initColor = node.fill.nodeValue;
+          node.fill.nodeValue = 'red';
+          node.oscillator.start(0);
           setTimeout(() => {
-            oscillator.stop();
-          }, speed / 2);
+            node.fill.nodeValue = initColor;
+            node.oscillator.stop();
+          }, delay / 2);
         }
-      }, speed * j);
+      }, delay * j);
     })(i += 1);
-  }
+}
 
-  function createOscillators(context, levels) {
-    return levels.map((freq) => {
+  function createOscillators(context, week) {
+    return week.map((day) => {
+      const freq = levels[day.attributes.fill.nodeValue];
       const oscillator = context.createOscillator();
       const gainNode = context.createGain();
       gainNode.gain.value = 0.1;
@@ -38,8 +38,11 @@
       oscillator.frequency.value = freq;
       oscillator.connect(gainNode);
       gainNode.connect(context.destination);
-      return oscillator;
+      return {
+        oscillator,
+        fill: day.attributes.fill
+      };
     });
   };
 
-})(new AudioContext, 50, 200);
+})(new AudioContext, 440, 100);
